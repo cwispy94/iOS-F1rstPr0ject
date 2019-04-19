68536adf1c68fd7588eef9061be210d9f56d479d
Ryan <stephen.kellogg94@gmail.com>
2019-04-19 15:11:08 -0400

    Base application

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # iOS-F1rstPr0ject
 Learning iOS with friends
+
+Two Branches Exist: Ryan's and Liz's :)
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Ryan Gross on 4/19/19.
+//  Copyright © 2019 Liz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
