0d3d82245f225506983edc6edf43ad345ac3febf
Ryan <stephen.kellogg94@gmail.com>
2019-04-19 16:03:11 -0400

    Added button

---
+++ MyFirstApp/InitialViewController.swift
@@ -0,0 +1,32 @@
+//
+//  InitialViewController.swift
+//  MyFirstApp
+//
+//  Created by Ryan Gross on 4/19/19.
+//  Copyright © 2019 Liz. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+    @IBOutlet var vibrationButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
